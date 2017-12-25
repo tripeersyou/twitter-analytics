f57cb070b646e895fbda9e62721d393b87ff7193
Francis Avancena <foavancena@gmail.com>
2017-12-25 17:21:16 +0800

    Added new API endpoint

--- routes/twitter.js
+++ routes/twitter.js
@@ -6,43 +6,69 @@ const router = express.Router();
 const TwitAPI = new Twit(config);
 
 // Search for Tweets
-router.get('/tweets',(request,response)=>{
-    TwitAPI.get('search/tweets',{q: request.query.q, count: request.query.count},(error, data) => {
-        if (error) {
-            response.status(500).json(error);
-        } else {
-            response.status(200).json(data.statuses);
-        }
-    });
+router.get('/tweets', (request, response) => {
+  TwitAPI.get('search/tweets', {
+    q: request.query.q,
+    count: request.query.count
+  }, (error, data) => {
+    if (error) {
+      response.status(500).json(error);
+    } else {
+      response.status(200).json(data.statuses);
+    }
+  });
 });
 
 // Get user followers
-router.get('/followers/:handle',(request, response)=>{
-    TwitAPI.get('followers/list',{screen_name: request.params.handle, count: request.query.count}, (error, data) => {
-        if (error){
-            response.status(500).json(error)
-        } else {
-            response.status(200).json(data.users);
-        }
-    });
+router.get('/followers/:handle', (request, response) => {
+  TwitAPI.get('followers/list', {
+    screen_name: request.params.handle,
+    count: request.query.count
+  }, (error, data) => {
+    if (error) {
+      response.status(500).json(error)
+    } else {
+      response.status(200).json(data.users);
+    }
+  });
 });
 
 // Get user
-router.get('/user/:handle', (request, response)=>{
-    TwitAPI.get('users/show',{screen_name: request.params.handle}, (error, data)=>{
-        response.status(200).json(data);
-    });
+router.get('/user/:handle', (request, response) => {
+  TwitAPI.get('users/show', {
+    screen_name: request.params.handle
+  }, (error, data) => {
+    response.status(200).json(data);
+  });
 });
 
 // Get user following
-router.get('/following/:handle',(request, response)=>{
-    TwitAPI.get('friends/list',{screen_name: request.params.handle, count: request.query.count},(error,data)=>{
-        if (error) {
-            response.status(500).json(error);
-        } else {
-            response.status(200).json(data.users);
-        }
-    });
+router.get('/following/:handle', (request, response) => {
+  TwitAPI.get('friends/list', {
+    screen_name: request.params.handle,
+    count: request.query.count
+  }, (error, data) => {
+    if (error) {
+      response.status(500).json(error);
+    } else {
+      response.status(200).json(data.users);
+    }
+  });
+});
+
+// Get user tweets
+router.get('/statuses/:handle', (request, response) => {
+  TwitAPI.get('statuses/user_timeline', {
+    screen_name: request.params.handle,
+    count: request.query.count
+  }, (error, data) => {
+    if (error) {
+      response.status(500).json(error);
+    } else {
+      response.status(200).json(data);
+      1
+    }
+  });
 });
 
 module.exports = router;
